a0dba2aa4c4846f32553fa5c6e56853b9c05e1b4
Muhammad Talha <mtalhajawad739@gmail.com>
2026-03-06 20:23:41 +0500

    Update memory_game.js

--- memory_game.js
+++ memory_game.js
@@ -1,121 +1,32 @@
-$(document).ready(function(){
+function check_match(){
 
-    let symbols = ["🍎","🍎","🍌","🍌","🍇","🍇","🍓","🍓"];
+    if(first_card.attr("data_symbol") == second_card.attr("data_symbol")){
 
-    let first_card = null;
-    let second_card = null;
-    let lock_board = false;
+        matched_pairs++;
 
-    let moves = 0;
-    let matched_pairs = 0;
+        reset_cards();
 
-    let timer = 0;
-    let timer_started = false;
-    let timer_interval;
+        if(matched_pairs == symbols.length/2){
 
-    
-    shuffle_cards();
-    create_board();
+            clearInterval(timer_interval);
 
- 
-    function shuffle_cards(){
-        symbols.sort(() => Math.random() - 0.5);
-    }
+            $("#win_message").fadeIn();
 
-    
-    function create_board(){
-        for(let i=0;i<symbols.length;i++){
-            let card = $("<div></div>");
-            card.addClass("card");
-            card.attr("data_symbol", symbols[i]);
-            card.text("?");
-            $("#game_board").append(card);
-        }
-    }
-
-  
-    function start_timer(){
-        timer_interval = setInterval(function(){
-            timer++;
-            $("#timer").text(timer);
-        },1000);
-    }
-
-   
-    $(document).on("click",".card",function(){
-
-        if(lock_board) return;
-        if($(this).hasClass("flipped")) return;
-
-        if(!timer_started){
-            start_timer();
-            timer_started = true;
         }
 
-        $(this).addClass("flipped");
-        $(this).text($(this).attr("data_symbol"));
+    }
+    else{
 
-        if(first_card == null){
-            first_card = $(this);
-        } else {
-            second_card = $(this);
-            lock_board = true;
+        setTimeout(function(){
 
-            moves++;
-            $("#moves").text(moves);
+            first_card.removeClass("flipped").text("?");
 
-            check_match();
-        }
-    });
+            second_card.removeClass("flipped").text("?");
 
-    
-    function check_match(){
-        if(first_card.attr("data_symbol") == second_card.attr("data_symbol")){
-            $("#match_sound")[0].play();
-            matched_pairs++;
             reset_cards();
 
-            if(matched_pairs == symbols.length/2){
-                clearInterval(timer_interval); 
-                $("#win_message").fadeIn();
-            }
+        },1000);
 
-        } else {
-            $("#wrong_sound")[0].play();
-            setTimeout(function(){
-                first_card.removeClass("flipped").text("?");
-                second_card.removeClass("flipped").text("?");
-                reset_cards();
-            },1000);
-        }
     }
 
-    function reset_cards(){
-        first_card = null;
-        second_card = null;
-        lock_board = false;
-    }
-
-   
-    $("#restart_button").click(function(){
-        first_card = null;
-        second_card = null;
-        lock_board = false;
-        moves = 0;
-        matched_pairs = 0;
-        timer = 0;
-        timer_started = false;
-
-        $("#moves").text(moves);
-        $("#timer").text(timer);
-        $("#win_message").hide();
-        $("#game_board").empty();
-
-        shuffle_cards();
-        create_board();
-
-        clearInterval(timer_interval);
-    });
-
-
-});
+}
